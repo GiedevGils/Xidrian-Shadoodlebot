48f44255e80135b88c1191571efafbfed30ff74b
Giede van Gils <giedevangils@gmail.com>
2019-10-07 22:52:32 +0200

    Finish followage command

--- src/commands/external_commands/twitch/followAge.ts
+++ src/commands/external_commands/twitch/followAge.ts
@@ -1,17 +1,17 @@
 import { ExternalCommand } from '../externalCommand';
+import settings from '../../../config/settings.json';
 
 export class FollowAgeCommand extends ExternalCommand {
     public commandName: string = 'followage';
     public commandDescription: string = 'This command lists the amount of time a user has been following.';
 
-    public run(client: any,args: any) {
+    public run(client: any, args: any) {
+        let url = settings["2gbe_api"] + 'following.php?user=$' + args.info.user['display-name'] + '&channel=$xidrian&format=mwdhms';
 
-        let url = 
-        
-        
-        this.sendGetRequest()
-
-        client.say(args.info.channel, '');
-
+        this.sendGetRequest(url)
+            .then( (res: any) => {
+                client.say(args.info.channel, "@" + res + ".");
+            })
+            .catch(err => console.log(err));
     }
 }
