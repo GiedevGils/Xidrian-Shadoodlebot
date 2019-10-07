b7777be171f50f5a3ac0df352383be29adcb9ddf
Giede van Gils <giedevangils@gmail.com>
2019-10-07 22:52:32 +0200

    Finish followage command

--- src/commands/enabledCommands.ts
+++ src/commands/enabledCommands.ts
@@ -3,8 +3,10 @@ import { RulesCommand } from './general/rules';
 import { CommandsCommand } from './general/commands';
 import { LevelCommand } from './external_commands/league/level';
 import { RankCommand } from './external_commands/league/rank';
+import { FollowAgeCommand } from './external_commands/twitch/followAge';
 
 const commands = [GreetCommand, RulesCommand, CommandsCommand, // General commands
-                    LevelCommand, RankCommand] // League Commands
+                    LevelCommand, RankCommand,                 // League Commands
+                    FollowAgeCommand ]                         // Twitch Commands
 
 export const enabledCommands = commands.map( command => new command() );
